3133f7f8dfc90e10adc973f27303f648a9ac589c
Jakub Rybiński <jakub.rybinski@tsh.io>
2019-08-27 12:12:29 +0200

    [RegistrationForm] Create form group and connect it with template

--- src/app/form-field-overview-example.ts
+++ src/app/form-field-overview-example.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { UsernameService } from './username.service';
 import { UserService } from './user.service';
-import { FormBuilder } from '@angular/forms';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'form-field-overview-example',
@@ -9,10 +9,24 @@ import { FormBuilder } from '@angular/forms';
   styleUrls: ['form-field-overview-example.css'],
 })
 export class FormFieldOverviewExample {
+  registrationForm: FormGroup;
+
   constructor(
     private _user: UserService, 
     private _formBuilder: FormBuilder,
     private _username: UsernameService, 
-  ) {}
+  ) {
+    this.registrationForm = this._formBuilder.group( {
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      username: ['', Validators.required],
+      description: [''],
+      gender: ['', Validators.required],
+    })
+  }
+
+  onSubmit() {
+    console.log('Submited ', this.registrationForm.getRawValue());
+  }
 }
 
